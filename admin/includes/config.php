a242580efccc0a2175b76d4d0f0fc710bd1cfa2f
Gerardo <the.z0w@gmail.com>
2025-04-08 10:09:00 -0600

    add layoouts

---
+++ admin/includes/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', 'localhost');          // Set database host
+define('DB_USER', 'u104624554_franke');             // Set database user
+define('DB_PASS', 'z0wrceGer!!_');             // Set database password
+define('DB_NAME', 'u104624554_franke');        // Set database name
---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,78 @@
+<?php
+//definir los permisos de usuario
+//obener el user level de tabla users
+$user_level = find_by_sql('SELECT user_level FROM users WHERE id =' . (int)$_SESSION['user_id']);
+$user_level = $user_level[0]['user_level'];
+?>
+
+<!-- Sidebar -->
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+            <span class="align-middle">SAE</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <!-- Dashboard -->
+            <li class="sidebar-header">Pages</li>
+
+            <li class="sidebar-item <?= $page == "home" ? "active" : ""; ?>">
+                <a class="sidebar-link" href="home.php"> <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span> </a>
+            </li>
+            <!-- Fin Dashboard -->
+
+            <?php if ($user_level <= 2) { ?>
+                <!-- Clientes -->
+                <li class="sidebar-header">Embarques</li>
+                <li class="sidebar-item <?= $separador == "embarques" ? "active" : ""; ?>">
+                    <a data-bs-target="#embarques_menu" data-bs-toggle="collapse" class="sidebar-link <?= $separador == "embarques" ? "" : "collapsed"; ?>"><i class="align-middle" data-feather="truck"></i>Embarques</a>
+                    <ul id="embarques_menu" class="sidebar-dropdown list-unstyled collapse <?= $separador == "embarques" ? "show" : ""; ?>">
+                        <li class="sidebar-item <?= $page == "embarque" ? "active" : ""; ?>">
+                            <a class="sidebar-link" href="./embarques.php">Lista de Embarques</a>
+                        </li>
+
+                    </ul>
+                </li>
+            <?php
+            }
+            ?>
+
+            <?php
+            //mostrar solo si el usuario es administrador
+            if ($user_level == 1) { ?>
+                <!-- Menu HR -->
+                <li class="sidebar-header">Administracion</li>
+
+                <li class="sidebar-item <?= $separador == "Usuarios" ? "active" : ""; ?>">
+                    <a data-bs-target="#HR_menu" data-bs-toggle="collapse" class="sidebar-link <?= $separador == "Usuarios" ? "" : "collapsed"; ?>"><i class="align-middle" data-feather="users"></i>Usuarios</a>
+                    <ul id="HR_menu" class="sidebar-dropdown list-unstyled collapse <?= $separador == "Usuarios" ? "show" : ""; ?>">
+                        <li class="sidebar-item <?= $page == "Usuarios" ? "active" : ""; ?>">
+                            <a class="sidebar-link" href="./usuarios.php">Gestion de Usuarios</a>
+                        </li>
+                    </ul>
+                </li>
+            <?php
+            }
+            ?>
+
+
+            <!-- Fin Menu Principal -->
+
+            <!-- Configuracion -->
+            <li class="sidebar-header">Configuracion</li>
+
+            <li class="sidebar-item <?= $page == "perfil" ? "active" : ""; ?>">
+                <a class="sidebar-link" href="perfil.php"> <i class="align-middle" data-feather="user"></i> <span class="align-middle">Perfil</span> </a>
+            </li>
+
+            <li class="sidebar-item <?= $page == "configuracion" ? "active" : ""; ?>">
+                <a class="sidebar-link" href="configuracion.php"> <i class="align-middle" data-feather="settings"></i> <span class="align-middle">Ajustes</span> </a>
+            </li>
+            <!-- Fin Configuracion -->
+
+
+        </ul>
+
+    </div>
+</nav>
+<!-- Fin Sidebar -->
