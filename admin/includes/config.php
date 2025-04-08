95141ece71ad7f13b3c23d91add7844dcd099990
Gerardo <the.z0w@gmail.com>
2025-04-08 10:17:30 -0600

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');          // Set database host
-define('DB_USER', 'u104624554_franke');             // Set database user
-define('DB_PASS', 'z0wrceGer!!_');             // Set database password
-define('DB_NAME', 'u104624554_franke');        // Set database name
+define('DB_USER', '');             // Set database user
+define('DB_PASS', '!!_');             // Set database password
+define('DB_NAME', '');        // Set database name
