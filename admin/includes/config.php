3985848ee8ff686674b46e4fe6f5dd96ec637307
Gerardo <the.z0w@gmail.com>
2025-04-08 10:18:12 -0600

    Delete admin/includes/config.php

--- admin/includes/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');          // Set database host
-define('DB_USER', '');             // Set database user
-define('DB_PASS', '!!_');             // Set database password
-define('DB_NAME', '');        // Set database name
